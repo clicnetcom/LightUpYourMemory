dda68f70b1f8005360a3816394744fa0bffd7608
clcnet <clicnet.com.br@gmail.com>
2024-12-16 10:43:19 -0300

    Adding custom button

---
+++ application/src/components/CustomButton.tsx
@@ -0,0 +1,70 @@
+
+
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { router } from 'expo-router'
+import { Button, Icon, MD3Theme, useTheme } from 'react-native-paper'
+
+export default function CustomButton({
+    path,
+    onPress,
+    label,
+    icon,
+    disabled = false,
+    buttonStyle = {}
+}: {
+    path?: string,
+    onPress?: Function,
+    label: string,
+    icon?: string,
+    disabled?: boolean,
+    buttonStyle?: any
+}) {
+
+    const theme = useTheme()
+    const styles = getStyles(theme)
+
+    return (
+        <Button
+            disabled={disabled}
+            icon={() => <Icon
+                color={theme.colors.onPrimary}
+                source={icon}
+                size={34}
+            />}
+            mode="contained"
+            onPress={() => {
+                if (onPress) {
+                    onPress()
+                }
+                if (path) {
+                    router.push(path)
+                }
+            }}
+            style={[styles.button, buttonStyle]}
+            contentStyle={styles.buttonContent}
+            labelStyle={styles.buttonLabel}
+        >
+            {label}
+        </Button >
+    )
+}
+const getStyles = (theme: MD3Theme) => StyleSheet.create({
+    button: {
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+
+    buttonContent: {
+        justifyContent: 'center',
+        flexDirection: 'row-reverse',
+
+    },
+    buttonLabel: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        marginTop: 18,
+        paddingVertical: 12,
+        width: '80%',
+    },
+})
