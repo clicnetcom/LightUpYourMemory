0566b12174a744d71518724db5d61a899bf81892
clcnet <clicnet.com.br@gmail.com>
2024-12-17 15:57:48 -0300

    Adding storage functions

--- application/src/utils.ts
+++ application/src/utils.ts
@@ -1,5 +1,13 @@
+import { getDownloadURL, getStorage, ref } from "firebase/storage"
+
 export const formatTime = (time: number) => {
     const minutes = Math.floor(time / 60)
     const seconds = time % 60
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+}
+
+export const getStorageUrl = async (gsPath: string) => {
+    const storage = getStorage()
+    const fileRef = ref(storage, gsPath)
+    return await getDownloadURL(fileRef)
 }
