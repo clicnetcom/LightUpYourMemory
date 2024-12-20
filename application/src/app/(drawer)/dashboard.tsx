7fef6b361dd9737415f9ee6dd18564e11e76e7f8
clcnet <clicnet.com.br@gmail.com>
2024-12-20 08:35:48 -0300

    Adding dashboard info

--- application/src/app/(drawer)/dashboard.tsx
+++ application/src/app/(drawer)/dashboard.tsx
@@ -4,7 +4,7 @@ import { useTheme } from "@/useTheme"
 import { get, ref } from "firebase/database"
 import { useEffect, useState } from "react"
 import { View, ScrollView } from "react-native"
-import { ActivityIndicator, Text } from "react-native-paper"
+import { ActivityIndicator, Text, DataTable, Avatar } from "react-native-paper"
 
 export default function Dashboard() {
 
@@ -14,6 +14,25 @@ export default function Dashboard() {
 
     const [isAdmin, setIsAdmin] = useState(false)
     const [isLoading, setIsLoading] = useState(true)
+
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        get(ref(database, 'users')).then(async (snapshot) => {
+            if (snapshot.exists()) {
+                const data = snapshot.val()
+                const users = Object.values(data).map((user: any, index) => {
+                    user.id = Object.keys(data)[index]
+                    return user
+                })
+                setUsers(users)
+            }
+        }).catch((error) => {
+            console.error("Error fetching users:", error)
+        })
+
+    }, [])
 
     useEffect(() => {
         const userRef = ref(database, `users/${user?.uid}/isAdmin`)
@@ -26,7 +45,7 @@ export default function Dashboard() {
             }
         })
     }, [])
-
+    console.log('users', users)
 
     if (isLoading) {
         return (
@@ -66,15 +85,42 @@ export default function Dashboard() {
             style={{
                 backgroundColor: theme.colors.background,
                 flex: 1,
+                padding: 16,
             }}
         >
-
             <ScrollView>
+                <DataTable>
+                    <DataTable.Header>
+                        <DataTable.Title>Photo</DataTable.Title>
+                        <DataTable.Title>Email</DataTable.Title>
+                        <DataTable.Title>Name</DataTable.Title>
+                        <DataTable.Title>Id</DataTable.Title>
+                        <DataTable.Title numeric>Admin</DataTable.Title>
+                        <DataTable.Title numeric>Plays</DataTable.Title>
+                    </DataTable.Header>
+                    {users.map((user: any, index: number) => (
+                        <DataTable.Row key={index}>
+                            <DataTable.Cell>
+                                {user.photo &&
+                                    <Avatar.Image size={50} source={{ uri: user.photoURL }} style={{ marginBottom: 8 }} />
+                                }
+                                {!user.photo &&
+                                    <Avatar.Icon size={50} icon={"account"} style={{ marginBottom: 8 }} />
+                                }
+                            </DataTable.Cell>
+                            <DataTable.Cell>{user.email || 'N/A'}</DataTable.Cell>
+                            <DataTable.Cell>{user.name || 'N/A'}</DataTable.Cell>
+                            <DataTable.Cell>{user.id || 'N/A'}</DataTable.Cell>
+                            <DataTable.Cell numeric>
+                                {user.isAdmin ? 'Yes' : 'No'}
+                            </DataTable.Cell>
 
-                <Text variant="headlineMedium">
-                    about the application
-                </Text>
-
+                            <DataTable.Cell numeric>
+                                {user.stats?.plays || 0}
+                            </DataTable.Cell>
+                        </DataTable.Row>
+                    ))}
+                </DataTable>
             </ScrollView>
         </View>
     )
